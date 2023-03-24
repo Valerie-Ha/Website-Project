f1b295cf5f0b40b7e77b9e0443357d43b1df6eed
Valerie Pham <111666452+Valerie-Ha@users.noreply.github.com>
2023-03-24 02:50:23 +0000

    Fri, Mar 24, 2023, 2:50 AM +00:00

--- java/page1.js
+++ java/page1.js
@@ -1,7 +1,11 @@
 const instaTxt = document.querySelector('.instaTxt')
 const instaBtn = document.getElementById('insta')
+
 const tikTxt = document.querySelector('.tikTxt')
 const tikTokBtn = document.getElementById('tiktok')
+
+const youTxt = document.querySelector('.youTxt')
+const youBtn = document.getElementById('youtube')
 
 instaBtn.addEventListener('mouseover', () => {
     instaTxt.style.width = "90px"
@@ -17,4 +21,12 @@ tikTokBtn.addEventListener('mouseover', () => {
 })
 tikTokBtn.addEventListener('mouseout', () => {
     tikTxt.style.width = "0px"
+})
+
+
+youBtn.addEventListener('mouseover', () => {
+    youTxt.style.width = "80px"
+})
+youBtn.addEventListener('mouseout', () => {
+    youTxt.style.width = "0px"
 })
