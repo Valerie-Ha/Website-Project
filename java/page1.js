52be7ea6f2a93f14738a6fc298ee1a4e64b4a8dc
Valerie Pham <111666452+Valerie-Ha@users.noreply.github.com>
2023-03-27 21:06:47 +0000

    Mon, Mar 27, 2023, 9:06 PM +00:00

--- java/page1.js
+++ java/page1.js
@@ -47,8 +47,5 @@ twitchBtn.addEventListener('mouseout', () => {
 
 
 openBtn.addEventListener('click', () => {
-    openBtn.style.display = none
-    navContainer.classList.add('active')
-    navBar.classList.add('active')
-
+    
 })
